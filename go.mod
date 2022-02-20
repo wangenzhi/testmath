18f0c85c90dcd9c399dd606ed41e3187132700f6
wangenzhi <w_enzhi@163.com>
2022-02-20 13:55:45 +0800

    add testmath

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wangenzhi/testmath
+
+go 1.16
---
+++ math.go
@@ -0,0 +1,8 @@
+package testmath
+
+import "fmt"
+
+func Add(n1, n2 int) int {
+	fmt.Println("github.com/wangenzhi/testmath")
+	return n1 + n2
+}
